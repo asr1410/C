3b99af6180ab6f37cd5fb1f7f663158ce330c34a
asr1410 <abhishek.rawat1410@gmail.com>
2021-01-03 10:20:40 +0530

    changes

---
+++ r1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    int var = 8;
+    char *str = "hi string %d\n";
+    printf(str, var);
+}
