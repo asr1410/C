b3f27d7d3ad44e763819ebb39879a9747a5c192e
asr1410 <abhishek.rawat1410@gmail.com>
2021-01-05 11:43:44 +0530

    changes

---
+++ fileIO2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    /*
+    YOu have to fill in values to a template letter.txt
+    //Lettr txt look like this:
+    thanks{{Name}}for purchasing {{items}}from outlet {{outlet}} .
+    please visit to outlet {{outlet}}for any kind problems
+    {{Name}}-->Aman
+    {{Item}}-->Bike
+    {{Outlet}}-->Yamaha
+    */
+    return 0;
+}
